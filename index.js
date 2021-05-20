ee54e8bdc2a6be10538d8b196c25291648510494
Mahamud Hasan <imonmahamud495@gmail.com>
2021-05-20 23:16:39 +0600

    Learn How way work js-oop-concept

---
+++ index.js
@@ -0,0 +1,10 @@
+var rect={
+    width:100,
+    height:50,
+    draw:function(){
+        console.log('I am a Rectangle');
+        console.log('My width is :'+this.width);
+        console.log('My height is :'+this.height);        
+    }
+}
+rect.draw()
